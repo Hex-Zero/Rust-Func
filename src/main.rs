877161f1f071636b0d3e13e5a070afafeaeefdce
Ace Alrich <hex0@live.com>
2020-06-22 20:36:44 +0100

    loops

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,9 @@
-// mod pointer;
-mod structs;
-
 fn main() {
-    structs::run();
+    let mut prime = [true; 31];
+
+    let mut p = 2;
+
+    for p in &prime {
+        println!("{}", p);
+    }
 }
-
-// fn greet(greet: &str, name: &str) {
-//     print!("{} {}", greet, name);
-// }
-
-// fn add(n1: i32, n2: i32) -> i32 {
-//     n1 + n2
-// }
-// fn adder(){
-//     let get_sum = add(5, 5);
-//     println!("{}", get_sum);
-
-//     let add_nums = |n1: i32, n2: i32| n1 + n2;
-//     print!("{}", add_nums(3, 3));
-// }
