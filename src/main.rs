da81671693a8577bf2c04ff357c29d24368dc48d
Ace Alrich <hex0@live.com>
2020-06-24 23:31:00 +0100

    imp debug

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,21 @@
-use std::iter::Iterator;
-
 fn main() {
-    let mut prime = [true; 31];
+    println!("start");
+    let mut prime = [true; 30000];
 
     let mut p = 2;
-    println!("{}", &prime[4])
+    while p * p < 30000 {
+        if prime[p] {
+            let mut i = p * p;
+            while i < 30000 {
+                prime[i] = false;
+                i += p
+            }
+        }
+        p += 1;
+    }
+    let mut index = 0;
+    for item in prime.iter() {
+        println!("{}:{}", index, item);
+        index += 1;
+    }
 }
